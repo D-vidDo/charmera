60fe755781d9c04d75128b55a459e7ed18038992
David Do <david-do1@outlook.com>
2026-01-12 14:02:06 -0700

    style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Upload from './pages/Upload.jsx'
-import Header from './components/Header.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Upload from "./pages/Upload"
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-      </Routes>
+      <div className="min-h-screen flex justify-center bg-white">
+        <div className="w-full max-w-6xl px-4">
+          <Header />
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/upload" element={<Upload />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   )
 }
