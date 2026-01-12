397abe94f40b32fe04c6e5dcf8c83ceacac5aadb
David Do <david-do1@outlook.com>
2026-01-12 13:32:38 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Upload from './pages/Upload'
 
 function App() {
   return (
-    <div>
-      <h1>Hello, Charmera!</h1>
-    </div>
     <BrowserRouter>
-      <nav className="p-4 flex justify-between">
-        <Link to="/">Home</Link>
-        <Link to="/upload">Upload</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/upload" element={<Upload />} />
@@ -22,4 +14,3 @@ function App() {
 }
 
 export default App
-
