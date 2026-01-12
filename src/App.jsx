94847efca12bf67d468f1a0473409d47155d012d
David Do <david-do1@outlook.com>
2026-01-12 13:44:05 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Upload from './pages/Upload'
+import Home from './pages/Home.jsx'
+import Upload from './pages/Upload.jsx'
 
 function App() {
   return (
